b02498eb5ace91cb5e1da3b9f04d897dfc3e37fb
Lucas Allen <lucas.g.allen@gmail.com>
2017-05-12 11:45:19 +0300

    Shift cell data to Board component. Send action up from Cell components to be handled by Board.

--- src/index.js
+++ src/index.js
@@ -12,28 +12,31 @@ class Cell extends React.Component {
         };
     }
 
-    flipLifeState() {
-        this.setState({
-            isAlive: !this.state.isAlive
-        });
-    }
-
     render() {
+        const cellNum = this.props.cellNum;
+        const rowNum = this.props.rowNum;
+
         return (
-            <button className="cell" onClick={() => this.flipLifeState()}>
-                {this.state.isAlive ? 1 : 0}
+            <button className="cell" onClick={() => this.props.handleClick(rowNum, cellNum)}>
+                {this.props.isAlive ? 1 : 0}
             </button>
         );
     }
 }
 
 class Row extends React.Component {
-    renderCells(rowNum, cellsPerRow) {
+    renderCells(rowNum, cells) {
         const newRow = [];
 
-        for (let cellNum=0; cellNum<cellsPerRow; cellNum++) {
+        for (let cellNum=0; cellNum<cells.length; cellNum++) {
             newRow.push(
-                <Cell key={[rowNum,cellNum]}/>
+                <Cell
+                    key={[rowNum,cellNum]}
+                    isAlive={cells[cellNum]}
+                    cellNum={cellNum}
+                    rowNum={rowNum}
+                    handleClick={(rowNum, cellNum) => this.props.handleClick(rowNum,cellNum)}
+                />
             );
         }
 
@@ -41,25 +44,51 @@ class Row extends React.Component {
     }
 
     render() {
-        const cellsPerRow = 10;
+        const cells = this.props.cells;
         const rowId = this.props.id;
 
         return (
             <div className="cell-row">
-                {this.renderCells(rowId, cellsPerRow)}
+                {this.renderCells(rowId, cells)}
             </div>
         );
     }
 }
 
 class Board extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            rows: Array(10).fill(Array(10).fill(true))
+        }
+    }
+
+    flipLifeState(rowNum, cellNum) {
+        const rows = this.state.rows.slice();
+        const cellsInRow = rows[rowNum].slice();
+
+        cellsInRow[cellNum] = !cellsInRow[cellNum];
+        rows[rowNum] = cellsInRow;
+
+        this.setState({
+            rows: rows
+        });
+    }
+
     render() {
-        const cellRowCount = 10;
+        const cellRowCount = this.state.rows.length;
         const rows = [];
 
         for (let row=0; row<cellRowCount; row++) {
+            let cellsInRow = this.state.rows[row];
             rows.push(
-                <Row key={'row' + row} id={row} />
+                <Row
+                    key={'row' + row}
+                    id={row}
+                    cells={cellsInRow}
+                    handleClick={(row, cell) => this.flipLifeState(row, cell)}
+                />
             );
         }
 
@@ -72,6 +101,12 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    // Steps through each board state
+        // Check previous board state
+        // Set the new state of the board for each cell
+        // Save new state to old state
+        // Render board with new state
+
     render() {
         return (
             <div className="game">
