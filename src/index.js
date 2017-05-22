3c2d75359c770ce1a89742290596a2d235a8481e
Lucas Allen <lucas.g.allen@gmail.com>
2017-05-22 13:09:45 +0300

    Create board module for board utility functions.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const board =  require('./board.js');
 
 class Cell extends React.Component {
     render() {
@@ -144,7 +145,7 @@ class Game extends React.Component {
         const history = JSON.parse(JSON.stringify(this.state.history));
         const current = history[history.length - 1];
         const rows = current.cellRows.slice();
-        const newRows = getNewBoard(rows);
+        const newRows = board.getNewBoard(rows);
 
         this.setState({
             history: history.concat([{
@@ -185,154 +186,6 @@ class Game extends React.Component {
             </div>
         );
     }
-}
-
-function getNewBoard(cellRows) {
-    // Check cell neighbors to assign cell to die or live
-    // Returns new cellRows
-
-    const numRows = cellRows.length;
-
-    for (let i=0;i<numRows;i++) {
-        let cells = cellRows[i];
-        let numCells = cells.length;
-
-        for (let j=0;j<numCells;j++) {
-            let tempNghb = 0;
-
-            if (i-1>=0) {
-                if (cellRows[i-1][j].isAlive)
-                    tempNghb++;
-                if (j+1<numCells) {
-                    if (cellRows[i-1][j+1].isAlive) {
-                        tempNghb++;
-                    }
-                } else {
-                    if (cellRows[i-1][0].isAlive) {
-                        tempNghb++;
-                    }
-                }
-            } else {
-                if (cellRows[numRows-1][j].isAlive) {
-                    tempNghb++;
-                }
-                if (j+1<numCells) {
-                    if (cellRows[numRows-1][j+1].isAlive) {
-                        tempNghb++;
-                    }
-                } else {
-                    if (cellRows[numRows-1][0].isAlive) {
-                        tempNghb++;
-                    }
-                }
-            }
-            if (j+1<numCells) {
-                if (cellRows[i][j+1].isAlive) {
-                    tempNghb++;
-                }
-                if (i+1<numRows) {
-                    if (cellRows[i+1][j+1].isAlive) {
-                        tempNghb++;
-                    }
-                } else {
-                    if (cellRows[0][j+1].isAlive) {
-                        tempNghb++;
-                    }
-                }
-            } else {
-                if (cellRows[i][0].isAlive) {
-                    tempNghb++;
-                }
-                if (i+1<numRows) {
-                    if (cellRows[i+1][0].isAlive) {
-                        tempNghb++;
-                    }
-                } else {
-                    if (cellRows[0][0].isAlive) {
-                        tempNghb++;
-                    }
-                }
-            }
-            if (i+1<numRows) {
-                if (cellRows[i+1][j].isAlive) {
-                    tempNghb++;
-                }
-                if (j-1>=0) {
-                    if (cellRows[i+1][j-1].isAlive) {
-                        tempNghb++;
-                    }
-                } else {
-                    if (cellRows[i+1][numCells-1].isAlive) {
-                        tempNghb++;
-                    }
-                }
-            } else {
-                if (cellRows[0][j].isAlive) {
-                    tempNghb++;
-                }
-                if (j-1>=0) {
-                    if (cellRows[0][j-1].isAlive) {
-                        tempNghb++;
-                    }
-                } else {
-                    if (cellRows[0][numCells-1].isAlive) {
-                        tempNghb++;
-                    }
-                }
-            }
-            if (j-1>=0) {
-                if (cellRows[i][j-1].isAlive) {
-                    tempNghb++;
-                }
-                if (i-1>=0) {
-                    if (cellRows[i-1][j-1].isAlive) {
-                        tempNghb++;
-                    }
-                } else {
-                    if (cellRows[numRows-1][j-1].isAlive) {
-                        tempNghb++;
-                    }
-                }
-            } else {
-                if (cellRows[i][numCells-1].isAlive) {
-                    tempNghb++;
-                }
-                if (i-1>=0) {
-                    if (cellRows[i-1][numCells-1].isAlive) {
-                        tempNghb++;
-                    }
-                } else {
-                    if (cellRows[numRows-1][numCells-1].isAlive) {
-                        tempNghb++;
-                    }
-                }
-            }
-            cellRows[i][j].nghbrCount = tempNghb;
-        }
-    }
-
-    // Using nghbrCount, set isAlive according to
-    // the Game of Life rules
-    for (let i=0;i<numRows;i++) {
-        let numCells = cellRows[i].length;
-
-        for (let j=0;j<numCells;j++) {
-            if (cellRows[i][j].isAlive) {
-                if (cellRows[i][j].nghbrCount === 2 || cellRows[i][j].nghbrCount === 3) {
-                    cellRows[i][j].isAlive = true;
-                } else {
-                    cellRows[i][j].isAlive = false;
-                }
-            } else {
-                if (cellRows[i][j].nghbrCount === 3) {
-                    cellRows[i][j].isAlive = true;
-                }
-            }
-        }
-    }
-
-
-    return cellRows;
 }
 
 ReactDOM.render(
